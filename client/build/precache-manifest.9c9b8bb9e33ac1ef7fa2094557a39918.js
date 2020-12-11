c493f38d2ea713afdd393b8e5b5ba5149567eb02
jamiemcg10 <jamie.smart.10@gmail.com>
2020-12-11 15:06:29 -0500

    added build folder

---
+++ client/build/precache-manifest.9c9b8bb9e33ac1ef7fa2094557a39918.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929e88bf395c0ee6001e80e928a06ead",
+    "url": "/client/build/index.html"
+  },
+  {
+    "revision": "c852614ceaa399a46177",
+    "url": "/client/build/static/css/main.5dd74a01.chunk.css"
+  },
+  {
+    "revision": "e2208f6a0f4cd0b2d6e9",
+    "url": "/client/build/static/js/2.46a90373.chunk.js"
+  },
+  {
+    "revision": "b169a77a5193c9019e48b593d06e805c",
+    "url": "/client/build/static/js/2.46a90373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c852614ceaa399a46177",
+    "url": "/client/build/static/js/main.7c59c038.chunk.js"
+  },
+  {
+    "revision": "06c262fe8382814656f3",
+    "url": "/client/build/static/js/runtime-main.0c6a9313.js"
+  },
+  {
+    "revision": "1afe94589bff8fd847726eb7955d1eb8",
+    "url": "/client/build/static/media/facebook-logo.1afe9458.png"
+  }
+]);
