0928453334c9215dac1b5d51d9aaafdb57c135c1
jamiemcg10 <jamie.smart.10@gmail.com>
2024-09-09 22:53:02 -0400

    clean up header

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -4,24 +4,20 @@ import Link from 'next/link'
 import Logo from './Logo'
 import { useContext } from 'react'
 import { SessionContext } from '../utils/Session'
-// import { SessionContext } from '../utils/Session'
-// import supabase from '../utils/supabase'
 
 function getLogoLink(loggedIn: boolean) {
-  return loggedIn ? '/main' : './'
+  return loggedIn ? '/cookbook' : './'
 }
 
 export default function Header() {
   const session = useContext(SessionContext)
-  console.log({ session })
+  console.log({ session }) // name should come from the user
   const loggedIn = !!session
-
-  console.log(session)
 
   const greeting = session?.displayName ? `Hi ${session.displayName}!` : 'Hi!'
 
   return (
-    <div className="bg-mvc-green flex h-20 items-center justify-between px-5">
+    <div className="bg-mvc-green flex h-16 items-center justify-between px-5">
       <Link href={getLogoLink(loggedIn)}>
         <Logo small />
       </Link>
