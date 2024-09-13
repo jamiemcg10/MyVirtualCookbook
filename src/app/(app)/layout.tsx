1889c4d376bc2ebeffd1dca3ca6806eaa5cc6efd
jamiemcg10 <jamie.smart.10@gmail.com>
2024-09-12 20:28:44 -0400

    remove useEffect from layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Session from '../utils/Session'
 import Header from '../ui/Header'
 
@@ -9,13 +9,6 @@ interface AppProps {
 
 export default function App({ children }: AppProps) {
   const [user, setUser] = useState(undefined)
-
-  useEffect(() => {
-    console.log({ user })
-    if (user) {
-      console.log('there is a user') // redirect if home or at login not at about page
-    }
-  }, [user])
 
   return (
     <Session session={user} setSession={setUser}>
