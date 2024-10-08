544405386df3004bc084d051670e0405829dc34f
jamiemcg10 <jamie.smart.10@gmail.com>
2024-10-08 12:46:17 -0400

    start adding footer and cookbook states

--- src/app/(app)/cookbook/page.tsx
+++ src/app/(app)/cookbook/page.tsx
@@ -5,10 +5,23 @@ import React, { useContext, useEffect, useState } from 'react'
 import { getCookbook } from '../../utils/cookbook'
 import { SessionContext } from '@/app/utils/Session'
 import { ChapterWithRecipeNotes } from '@/app/lib/types'
+import Footer from '@/app/ui/Footer'
+import { CircularProgress, ThemeProvider } from '@mui/material'
+import { theme } from '@/app/ui/.theme/theme'
+
+declare module '@mui/material/CircularProgress' {
+  // eslint-disable-next-line no-unused-vars
+  interface CircularProgressPropsColorOverrides {
+    'mvc-green': true
+    'mvc-yellow': true
+    'mvc-white': true
+    'mvc-gray': true
+  }
+}
 
 export default function Cookbook() {
   const user = useContext(SessionContext)
-  const [cookbook, setCookbook] = useState<ChapterWithRecipeNotes[]>([])
+  const [cookbook, setCookbook] = useState<ChapterWithRecipeNotes[] | null>(null)
 
   useEffect(() => {
     window.history.replaceState(null, '', '/cookbook')
@@ -18,11 +31,26 @@ export default function Cookbook() {
 
   return (
     <>
-      <div className="p-8 flex flex-col space-y-2">
-        {cookbook.map((chapter) => {
-          return <CookbookChapter chapter={chapter} key={chapter.id} />
-        })}
-      </div>
+      {cookbook ? (
+        <>
+          <div className="p-8 flex flex-col space-y-2 grow">
+            {cookbook.length ? (
+              cookbook.map((chapter) => {
+                return <CookbookChapter chapter={chapter} key={chapter.id} />
+              })
+            ) : (
+              <div>Your cookbook is empty. Add chapters and recipes to get started.</div>
+            )}
+          </div>
+          <Footer />
+        </>
+      ) : (
+        <div className="grow justify-center items-center flex">
+          <ThemeProvider theme={theme}>
+            <CircularProgress size="65px" color="mvc-yellow" />
+          </ThemeProvider>
+        </div>
+      )}
     </>
   )
 }
---
+++ src/app/ui/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div
+      className="h-14 bg-[rgba(5,28,49,.9)]"
+      style={{
+        boxShadow: '0 1px 50px 1px rgb(0 0 0 / 0.8)'
+      }}></div>
+  )
+}
