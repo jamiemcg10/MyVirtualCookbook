b15072b76d07bb0faf5df14edce6f50fd71e6731
Jamie Smart <jamie.smart.10@gmail.com>
2025-04-23 19:23:29 -0400

    update theme

--- src/app/ui/.theme/theme.ts
+++ src/app/ui/.theme/theme.ts
@@ -1,5 +1,18 @@
 import { createTheme, alpha, getContrastRatio } from '@mui/material/styles'
 
+function computeVariants(variant: 'text' | 'outlined' | 'contained') {
+  return (Object.keys(palette) as Array<keyof typeof palette>).map((key) => {
+    return {
+      props: { variant: variant as 'text' | 'outlined' | 'contained', color: key },
+      style: {
+        '&:hover': {
+          backgroundColor: alpha(palette[key].main, 0.3)
+        }
+      }
+    }
+  })
+}
+
 const greenBase = '#6aa84f'
 const greenMain = alpha(greenBase, 0.7)
 
@@ -12,34 +25,41 @@ const whiteMain = alpha(whiteBase, 0.7)
 const grayBase = '#808080'
 const grayMain = alpha(grayBase, 0.7)
 
+const palette = {
+  'mvc-green': {
+    main: greenMain,
+    light: alpha(greenBase, 0.5),
+    dark: alpha(greenBase, 0.9),
+    contrastText: getContrastRatio(greenMain, '#fff') > 4.5 ? '#fff' : '#303030'
+  },
+  'mvc-yellow': {
+    main: yellowMain,
+    light: alpha(yellowBase, 0.5),
+    dark: alpha(yellowBase, 0.9),
+    contrastText: getContrastRatio(yellowMain, '#fff') > 4.5 ? '#fff' : '#303030'
+  },
+  'mvc-white': {
+    main: whiteMain,
+    light: alpha(whiteBase, 0.5),
+    dark: alpha(whiteBase, 0.9),
+    contrastText: '#303030'
+  },
+  'mvc-gray': {
+    main: grayMain,
+    light: alpha(grayBase, 0.5),
+    dark: alpha(grayBase, 0.9),
+    contrastText: '#303030' // not working
+  }
+}
+
 export const theme = createTheme({
   typography: {
     fontFamily: 'inherit'
   },
-  palette: {
-    'mvc-green': {
-      main: greenMain,
-      light: alpha(greenBase, 0.5),
-      dark: alpha(greenBase, 0.9),
-      contrastText: getContrastRatio(greenMain, '#fff') > 4.5 ? '#fff' : '#303030'
-    },
-    'mvc-yellow': {
-      main: yellowMain,
-      light: alpha(yellowBase, 0.5),
-      dark: alpha(yellowBase, 0.9),
-      contrastText: getContrastRatio(yellowMain, '#fff') > 4.5 ? '#fff' : '#303030'
-    },
-    'mvc-white': {
-      main: whiteMain,
-      light: alpha(whiteBase, 0.5),
-      dark: alpha(whiteBase, 0.9),
-      contrastText: '#303030'
-    },
-    'mvc-gray': {
-      main: grayMain,
-      light: alpha(grayBase, 0.5),
-      dark: alpha(grayBase, 0.9),
-      contrastText: '#303030' // not working
+  components: {
+    MuiButton: {
+      variants: computeVariants('text')
     }
-  }
+  },
+  palette
 })
