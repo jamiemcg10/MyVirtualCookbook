1097fdf223dc4ad14852a2e598f7718c6d0ae53c
Jamie Smart <jamie.smart.10@gmail.com>
2025-04-24 16:05:18 -0400

    add notes firebase functions

--- src/app/utils/firebase/notes.ts
+++ src/app/utils/firebase/notes.ts
@@ -1,8 +1,12 @@
-import { collection } from 'firebase/firestore'
+import { collection, doc, setDoc } from 'firebase/firestore'
 import { db } from './firebase'
+import { Notes } from '@/app/lib/types'
 
 export const notes = (userId: string) => {
   return {
-    ref: collection(db, `users/${userId}/notes`)
+    ref: collection(db, `users/${userId}/notes`),
+    set: async (value: Notes) => {
+      await setDoc(doc(db, `users/${userId}/notes`, value.id), { notes: value })
+    }
   }
 }
