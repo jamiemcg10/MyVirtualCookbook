911d56bb932e0086b23cd11f3576657cf04e5a1a
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-05 14:32:06 -0400

    move notes to new component

---
+++ src/app/ui/CookbookNotes.tsx
@@ -0,0 +1,68 @@
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionActions from '@mui/material/AccordionActions'
+import cs from 'clsx'
+import ThemedButton from './buttons/ThemedButton'
+import { Chilanka } from 'next/font/google'
+import { useEffect, useRef, useState } from 'react'
+
+interface CookbookNotesProps {
+    notes: string
+    onSave?: (notes: string) => void
+}
+
+const chilanka = Chilanka({ weight: '400', preload: false })
+
+export default function CookbookNotes({notes}: CookbookNotesProps) {
+  const [editing, setEditing] = useState(false)
+  
+    const notesElRef = useRef<HTMLTextAreaElement | null>(null)
+
+    useEffect(() => {
+        if (notesElRef.current) notesElRef.current.innerText = notes
+      }, [])
+
+    return <>
+          <AccordionDetails className="py-0">
+        <textarea
+          rows={3}
+          className={cs(
+            'w-full text-md font-bold resize-none bg-yellow-100 rounded-md p-1 focus-visible:notes-focus-glow outline-none',
+            !editing && 'border-2 border-transparent pointer-events-none',
+            editing && 'border-2 border-mvc-green',
+            chilanka.className
+          )}
+          ref={notesElRef}></textarea>
+      </AccordionDetails>
+      <AccordionActions className="pt-0">
+        {!editing ? (
+          <ThemedButton
+            color="mvc-yellow"
+            onClick={() => {
+              setEditing(true)
+              notesElRef?.current?.focus()
+            }}>
+            Edit
+          </ThemedButton>
+        ) : (
+          <>
+            <ThemedButton
+              color="mvc-gray"
+              variant="outlined"
+              onClick={() => {
+                setEditing(false)
+              }}>
+              Cancel
+            </ThemedButton>
+            <ThemedButton
+              onClick={() => {
+                setEditing(false)
+                // nothing is currently being saved
+              }}
+              color="mvc-green">
+              Save
+            </ThemedButton>
+          </>
+        )}
+      </AccordionActions>
+    </>
+}
--- src/app/ui/CookbookRecipe.tsx
+++ src/app/ui/CookbookRecipe.tsx
@@ -1,19 +1,13 @@
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import Accordion from '@mui/material/Accordion'
 import AccordionSummary from '@mui/material/AccordionSummary'
-import AccordionDetails from '@mui/material/AccordionDetails'
-import AccordionActions from '@mui/material/AccordionActions'
-import { useState, useRef, useEffect, useContext } from 'react'
-import cs from 'clsx'
-import ThemedButton from './buttons/ThemedButton'
-import { Chilanka } from 'next/font/google'
+import { useContext } from 'react'
 import { RecipeWithNotes } from '../lib/types'
 import InlineInput from './inputs/InlineInput'
 import { users } from '../utils/firebase'
 import { SessionContext } from '../utils/Session'
 import { arrayRemove } from 'firebase/firestore'
-
-const chilanka = Chilanka({ weight: '400', preload: false })
+import CookbookNotes from './CookbookNotes'
 
 interface CookbookRecipeProps {
   recipe: RecipeWithNotes
@@ -34,13 +28,6 @@ export default function CookbookRecipe({ recipe }: CookbookRecipeProps) {
   }
 
   const user = useContext(SessionContext)
-
-  const [editing, setEditing] = useState(false)
-  const notesElRef = useRef<HTMLTextAreaElement | null>(null)
-
-  useEffect(() => {
-    if (notesElRef.current) notesElRef.current.innerText = notes
-  }, [])
 
   const { name, link, notes } = recipe
 
@@ -65,47 +52,7 @@ export default function CookbookRecipe({ recipe }: CookbookRecipeProps) {
           </a>
         </InlineInput>
       </AccordionSummary>
-      <AccordionDetails className="py-0">
-        <textarea
-          rows={3}
-          className={cs(
-            'w-full text-md font-bold resize-none bg-yellow-100 rounded-md p-1 focus-visible:notes-focus-glow outline-none',
-            !editing && 'border-2 border-transparent pointer-events-none',
-            editing && 'border-2 border-mvc-green',
-            chilanka.className
-          )}
-          ref={notesElRef}></textarea>
-      </AccordionDetails>
-      <AccordionActions className="pt-0">
-        {!editing ? (
-          <ThemedButton
-            color="mvc-yellow"
-            onClick={() => {
-              setEditing(true)
-              notesElRef?.current?.focus()
-            }}>
-            Edit
-          </ThemedButton>
-        ) : (
-          <>
-            <ThemedButton
-              color="mvc-gray"
-              variant="outlined"
-              onClick={() => {
-                setEditing(false)
-              }}>
-              Cancel
-            </ThemedButton>
-            <ThemedButton
-              onClick={() => {
-                setEditing(false)
-              }}
-              color="mvc-green">
-              Save
-            </ThemedButton>
-          </>
-        )}
-      </AccordionActions>
+      <CookbookNotes notes={notes} />
     </Accordion>
   )
 }
