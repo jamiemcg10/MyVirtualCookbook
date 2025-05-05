570ca35bb19242186d7b28b40f0e68ab121c92f2
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-05 17:44:27 -0400

    reformat LoginForm and add chapterOrder to new users

--- src/app/(app)/login/LoginForm.tsx
+++ src/app/(app)/login/LoginForm.tsx
@@ -4,12 +4,12 @@ import { TextField } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
 import clsx from 'clsx'
 import {
-  sendSignInLinkToEmail,
-  isSignInWithEmailLink,
-  signInWithEmailLink,
-  getAdditionalUserInfo,
-  GoogleAuthProvider,
-  signInWithPopup
+	sendSignInLinkToEmail,
+	isSignInWithEmailLink,
+	signInWithEmailLink,
+	getAdditionalUserInfo,
+	GoogleAuthProvider,
+	signInWithPopup
 } from 'firebase/auth'
 import Snackbar from '@mui/material/Snackbar'
 import LinkSignInButton from '../../ui/buttons/LinkSignInButton'
@@ -19,150 +19,152 @@ import { redirect } from 'next/navigation'
 import { SessionContext } from '../../utils/Session'
 
 export default function LoginForm() {
-  const [email, setEmail] = useState('')
-  const [errorText, setErrorText] = useState('')
-  const [showNotification, setShowNotification] = useState(false)
-  const session = useContext(SessionContext)
+	const [email, setEmail] = useState('')
+	const [errorText, setErrorText] = useState('')
+	const [showNotification, setShowNotification] = useState(false)
+	const session = useContext(SessionContext)
 
-  function handleSnackbarClose() {
-    setShowNotification(false)
-  }
+	function handleSnackbarClose() {
+		setShowNotification(false)
+	}
 
-  const sendMagicLink = async () => {
-    const actionCodeSettings = {
-      url: `${window.location.origin}/cookbook`,
-      handleCodeInApp: true
-    }
+	const sendMagicLink = async () => {
+		const actionCodeSettings = {
+			url: `${window.location.origin}/cookbook`,
+			handleCodeInApp: true
+		}
 
-    sendSignInLinkToEmail(auth, email, actionCodeSettings)
-      .then(() => {
-        window.localStorage.setItem('emailForSignIn', email)
-        setEmail('')
-        setShowNotification(true)
-      })
-      .catch(({ message }) => {
-        setErrorText(message)
-      })
-  }
+		sendSignInLinkToEmail(auth, email, actionCodeSettings)
+			.then(() => {
+				window.localStorage.setItem('emailForSignIn', email)
+				setEmail('')
+				setShowNotification(true)
+			})
+			.catch(({ message }) => {
+				setErrorText(message)
+			})
+	}
 
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider()
+	const signInWithGoogle = () => {
+		const provider = new GoogleAuthProvider()
 
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result)
-        // const token = credential?.accessToken
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				// const credential = GoogleAuthProvider.credentialFromResult(result)
+				// const token = credential?.accessToken
 
-        const user = { ...result, ...(getAdditionalUserInfo(result) || {}) }
-        const isNewUser = user.isNewUser
+				const user = { ...result, ...(getAdditionalUserInfo(result) || {}) }
+				const isNewUser = user.isNewUser
 
-        if (isNewUser) {
-          createUser(user)
-        }
+				if (isNewUser) {
+					createUser(user)
+				}
 
-        console.log({ user })
+				console.log({ user })
 
-        // redirect to cookbook
-        redirect('/cookbook')
-      })
-      .catch(({ message }) => {
-        setErrorText(message)
-      })
-  }
+				// redirect to cookbook
+				redirect('/cookbook')
+			})
+			.catch(({ message }) => {
+				setErrorText(message)
+			})
+	}
 
-  const createUser = async (user: any) => {
-    // move this to separate file, make a better type
-    const id = user.user.uid
+	const createUser = async (user: any) => {
+		// move this to separate file, make a better type
+		const id = user.user.uid
 
-    if (user.providerId === 'google.com') {
-      const displayName = user.profile.given_name
-      const pictureUrl = user.profile.picture
+		if (user.providerId === 'google.com') {
+			const displayName = user.profile.given_name
+			const pictureUrl = user.profile.picture
 
-      users(id).set({
-        id,
-        username: displayName,
-        pictureUrl
-      })
-    } else {
-      users(id).set({
-        id,
-        username: user.user.email
-      })
-    }
-  }
+			users(id).set({
+				id,
+				username: displayName,
+				pictureUrl,
+				chapterOrder: []
+			})
+		} else {
+			users(id).set({
+				id,
+				username: user.user.email,
+				chapterOrder: []
+			})
+		}
+	}
 
-  const submitBtnDisabled = email === ''
+	const submitBtnDisabled = email === ''
 
-  useEffect(() => {
-    if (session) {
-      redirect('/cookbook')
-    }
+	useEffect(() => {
+		if (session) {
+			redirect('/cookbook')
+		}
 
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-      let linkEmail = window.localStorage.getItem('emailForSignIn')
+		if (isSignInWithEmailLink(auth, window.location.href)) {
+			let linkEmail = window.localStorage.getItem('emailForSignIn')
 
-      if (!linkEmail) {
-        // TODO: implement asking user to confirm email
-        linkEmail = ''
-      }
+			if (!linkEmail) {
+				// TODO: implement asking user to confirm email
+				linkEmail = ''
+			}
 
-      signInWithEmailLink(auth, linkEmail, window.location.href)
-        .then((result) => {
-          window.localStorage.removeItem('emailForSignIn')
-          const user = { ...result, ...(getAdditionalUserInfo(result) || {}) }
+			signInWithEmailLink(auth, linkEmail, window.location.href)
+				.then((result) => {
+					window.localStorage.removeItem('emailForSignIn')
+					const user = { ...result, ...(getAdditionalUserInfo(result) || {}) }
 
-          const isNewUser = getAdditionalUserInfo(result)?.isNewUser
+					const isNewUser = getAdditionalUserInfo(result)?.isNewUser
 
-          if (isNewUser) {
-            createUser(user)
-          }
-        })
-        .catch((error) => {
-          setErrorText(error.message)
-          console.log({ error })
-        })
-    }
-  }, [session])
+					if (isNewUser) {
+						createUser(user)
+					}
+				})
+				.catch((error) => {
+					setErrorText(error.message)
+					console.log({ error })
+				})
+		}
+	}, [session])
 
-  return (
-    <>
-      <div className="px-5">
-        <h1 className="page-title">Log in</h1>
-        <div className="w-[22rem] p-9 shadow-xl bg-gray-300 rounded-lg m-auto login-container">
-          <h3 className="text-gray-700 text-center mb-6 text-xl">Log in to continue</h3>
-          <div>
-            <TextField
-              fullWidth
-              variant="outlined"
-              size="small"
-              className="mb-3"
-              id="email"
-              label="Email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <p
-              className={clsx(
-                'ml-0.5 mt-0.5 text-red-600 text-xs italic',
-                !errorText && 'invisible'
-              )}>
-              {errorText}
-            </p>
-            <div className="flex flex-col mt-4">
-              <LinkSignInButton disabled={submitBtnDisabled} onClick={sendMagicLink} />
-              <GoogleSignInButton onClick={signInWithGoogle} />
-            </div>
-          </div>
-        </div>
-      </div>
-      <Snackbar
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        autoHideDuration={3000}
-        message="Link sent to email"
-        open={showNotification}
-        onClose={handleSnackbarClose}
-      />
-    </>
-  )
+	return (
+		<>
+			<div className="px-5">
+				<h1 className="page-title">Log in</h1>
+				<div className="w-[22rem] p-9 shadow-xl bg-gray-300 rounded-lg m-auto login-container">
+					<h3 className="text-gray-700 text-center mb-6 text-xl">Log in to continue</h3>
+					<div>
+						<TextField
+							fullWidth
+							variant="outlined"
+							size="small"
+							className="mb-3"
+							id="email"
+							label="Email address"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<p
+							className={clsx(
+								'ml-0.5 mt-0.5 text-red-600 text-xs italic',
+								!errorText && 'invisible'
+							)}>
+							{errorText}
+						</p>
+						<div className="flex flex-col mt-4">
+							<LinkSignInButton disabled={submitBtnDisabled} onClick={sendMagicLink} />
+							<GoogleSignInButton onClick={signInWithGoogle} />
+						</div>
+					</div>
+				</div>
+			</div>
+			<Snackbar
+				anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+				autoHideDuration={3000}
+				message="Link sent to email"
+				open={showNotification}
+				onClose={handleSnackbarClose}
+			/>
+		</>
+	)
 }
