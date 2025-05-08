30718d6cae1318dd3de087ecdabf88e54480866b
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-08 17:10:33 -0400

    Google -> GCP

--- src/app/utils/firebase/firebaseAdmin.ts
+++ src/app/utils/firebase/firebaseAdmin.ts
@@ -2,11 +2,11 @@
 
 import * as admin from 'firebase-admin'
 
-function getGoogleCredentials() {
+function getGCPCredentials() {
   return {
-    projectId: process.env.GOOGLE_PROJECT_ID || '',
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replaceAll('\\n', '\n') || '',
-    clientEmail: process.env.GOOGLE_CLIENT_EMAIL || ''
+    projectId: process.env.GCP_PROJECT_ID || '',
+    private_key: process.env.GCP_PRIVATE_KEY?.replaceAll('\\n', '\n') || '',
+    clientEmail: process.env.GCP_SERVICE_ACCOUNT_EMAIL || ''
   }
 }
 
@@ -14,7 +14,7 @@ const firebaseApp = (
   admin.apps?.length
     ? admin.apps[0]
     : admin.initializeApp({
-        credential: admin.credential.cert(getGoogleCredentials())
+        credential: admin.credential.cert(getGCPCredentials())
       })
 ) as admin.app.App
 
