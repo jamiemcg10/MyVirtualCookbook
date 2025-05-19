b59710a9df66527c15f22d7457ebdaa1dfea15ca
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-19 13:56:41 -0400

    fix notes setting

--- src/app/utils/firebase/notes.ts
+++ src/app/utils/firebase/notes.ts
@@ -6,7 +6,7 @@ export const notes = (userId: string) => {
   return {
     ref: collection(db, `users/${userId}/notes`),
     set: async (value: Notes) => {
-      await setDoc(doc(db, `users/${userId}/notes`, value.id), { notes: value })
+      await setDoc(doc(db, `users/${userId}/notes`, value.id), value)
     },
     update: async (id: string, value: string) => {
       await updateDoc(doc(db, `users/${userId}/notes`, id), { notes: value })
