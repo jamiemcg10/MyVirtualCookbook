0a3c4728e10dff96af6cdd128885f6d9f1eacad6
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-21 10:45:48 -0400

    always show recipe menu on mobile

--- src/app/ui/RecipeMenu.tsx
+++ src/app/ui/RecipeMenu.tsx
@@ -17,7 +17,7 @@ export default function RecipeMenu({ onRename, onDelete }: RecipeMenuProps) {
       <div className="relative" onMouseLeave={() => setOpen(false)}>
         <IconButton
           size="small"
-          className="invisible group-hover:visible"
+          className="invisible group-hover:visible [@media(hover:none)]:visible"
           onClick={(e) => {
             e.stopPropagation()
             setOpen(!open)
