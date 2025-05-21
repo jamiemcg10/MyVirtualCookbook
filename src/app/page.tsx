b15e2cfd419788a23368bead911ec6156dc4f22b
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-21 14:00:03 -0400

    update landing mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ThemedButton from './ui/buttons/ThemedButton'
 export default function Home() {
   return (
     <div className="App">
-      <div className="text-center pt-32">
+      <div className="text-center pt-16 sm:pt-32">
         <Logo />
       </div>
       <div className="bg-mvc-green mt-10 mb-10 py-7 flex flex-wrap justify-around">
@@ -19,7 +19,10 @@ export default function Home() {
           <ThemedButton
             color="mvc-green"
             sx={{
-              margin: '1.25rem 5rem 2.5rem'
+              margin: {
+                xs: '2rem 2.5rem 2.5rem',
+                sm: '1.25rem 5rem 2.5rem'
+              }
             }}>
             Learn more
           </ThemedButton>
@@ -28,7 +31,10 @@ export default function Home() {
           <ThemedButton
             color="mvc-yellow"
             sx={{
-              margin: '1.25rem 5rem 2.5rem'
+              margin: {
+                xs: '2rem 2.5rem 2.5rem',
+                sm: '1.25rem 5rem 2.5rem'
+              }
             }}>
             Log in
           </ThemedButton>
