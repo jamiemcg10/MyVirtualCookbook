23a4584ef0ab389b792c1b8c88ce8152d905f197
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-29 00:18:13 -0400

    use async anonymous function

--- src/app/(app)/logout/Logout.tsx
+++ src/app/(app)/logout/Logout.tsx
@@ -1,19 +1,18 @@
+'use client'
+
 import { getAuth, signOut } from 'firebase/auth'
 import { redirect } from 'next/navigation'
-// import { useEffect } from 'react'
+import { useEffect } from 'react'
 
-export default async function Logout() {
-  // useEffect(async () => {
-  //   const auth = getAuth()
-  //   await signOut(auth)
+export default function Logout() {
+  useEffect(() => {
+    const auth = getAuth()
 
-  //   redirect('/login')
-  // }, [])
+    ;async () => {
+      await signOut(auth)
+    }
 
-  const auth = getAuth()
-  await signOut(auth)
-
-  redirect('/login')
-
+    redirect('/login')
+  }, [])
   return <></>
 }
