c23f82bdafaaf61f4588862294b92c019caf458d
Jamie Smart <jamie.smart.10@gmail.com>
2025-05-29 16:28:05 -0400

    remove empty dependency array from CookbookNotes

--- src/app/ui/CookbookNotes.tsx
+++ src/app/ui/CookbookNotes.tsx
@@ -15,7 +15,7 @@ export default function CookbookNotes({ notes, onSave }: CookbookNotesProps) {
 
   useEffect(() => {
     if (notesElRef.current) notesElRef.current.innerText = notes
-  }, [])
+  })
 
   return (
     <>
